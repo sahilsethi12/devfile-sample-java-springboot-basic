9c6639c316ff9371060e1c1d777e390be994693c
sasethi <sahil.sethi377@gmail.com>
2025-10-03 01:48:41 +0530

    updated

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,17 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.ImportResource;
 
 
-@RestController
 @SpringBootApplication
+@ImportResource({"classpath:spring/camel-context.xml"})
 public class DemoApplication {
 
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
-    }
-
+    // must have a main method spring-boot can run
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
