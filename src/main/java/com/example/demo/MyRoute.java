e09a0b5766b363108d11968e49efc0ba96ecc44f
sasethi <sahil.sethi377@gmail.com>
2025-10-03 01:23:24 +0530

    updated

---
+++ src/main/java/com/example/demo/MyRoute.java
@@ -0,0 +1,39 @@
+package com.example.sbcamelkafkaproducer;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyRoute extends RouteBuilder {
+
+    @Override
+    public void configure() {
+
+        restConfiguration()
+                .enableCORS(true)
+                .component("jetty")
+                .host("0.0.0.0")
+                .port(8988)
+                .bindingMode(RestBindingMode.json);
+
+        rest()
+                .get("/hello")
+                .to("direct:hello");
+
+
+        from("direct:hello")
+                .routeId("GreetingRoute")
+                .to("direct:greetStranger");
+
+        from("direct:greetStranger")
+                .routeId("greetStranger")
+                .setBody(simple("Hello Application!"));
+
+
+
+    }
+
+}
